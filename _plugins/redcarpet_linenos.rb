fac031366dc4d10847f7a9f8196e2245f6fefb09
Jean Helou <jean.helou@gmail.com>
2015-02-03 16:26:38 +0100

    remove line numbers

--- _plugins/redcarpet_linenos.rb
+++ _plugins/redcarpet_linenos.rb
@@ -1,21 +1,21 @@
-module Jekyll
-  module Converters
-    class Markdown
-      class RedcarpetParser
-        module WithPygments
-          include CommonMethods
-          def block_code(code, lang)
-            Jekyll::Deprecator.gracefully_require("pygments")
-            lang,options = lang && lang.split("|",2) || ["text",""]
-            linenos = !(options && options.include?("nolines"))
-            lang = lang && lang.split.first || "text"
-            add_code_tags(
-              Pygments.highlight(code, :lexer => lang, :options => { :encoding => 'utf-8', :linenos=> linenos}),
-              lang
-            )
-          end
-        end
-      end
-    end
-  end
-end
+#module Jekyll
+#  module Converters
+#    class Markdown
+#      class RedcarpetParser
+#        module WithPygments
+#          include CommonMethods
+#          def block_code(code, lang)
+#            Jekyll::Deprecator.gracefully_require("pygments")
+#            lang,options = lang && lang.split("|",2) || ["text",""]
+#            linenos = !(options && options.include?("nolines"))
+#            lang = lang && lang.split.first || "text"
+#            add_code_tags(
+#              Pygments.highlight(code, :lexer => lang, :options => { :encoding => 'utf-8', :linenos=> linenos}),
+#              lang
+#            )
+#          end
+#        end
+#      end
+#    end
+#  end
+#end
